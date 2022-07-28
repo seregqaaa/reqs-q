c4516501b9f2a8e2cf6672e27e2cf54f0eb52f4d
seregqa <sergei.kvashnin@sptovarov.dev>
2022-07-28 14:51:43 +0500

    update to v1.2.3

--- types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { RequestsQueue, RequestsQueueCore } from './core/RequestsQueue.js';
-import { QueueLogger } from './core/QueueLogger.js';
-import { QueueStoreManager, Stores } from './core/QueueStoreManager.js';
-import { RequestModel } from './models/Request.js';
-import { Priorities } from './global/constants.js';
-export { RequestModel, RequestsQueue, RequestsQueueCore, QueueLogger, QueueStoreManager, Stores, Priorities, };
-//# sourceMappingURL=index.d.ts.map
